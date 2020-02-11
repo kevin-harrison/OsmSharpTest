2bbcda2152184886d4126d2b7f69cca5876e76d9
Kevin Harrison <kevin@harrisoncentral.com>
2020-02-12 00:48:07 +0100

    Unity project commit

---
+++ Assets/OsmTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using OsmSharp.Streams;
+
+public class OsmTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        using (var fileStream = new FileInfo(@".\Assets\map.osm").OpenRead())
+        {
+            var source = new XmlOsmStreamSource(fileStream);
+            foreach (var element in source)
+            {
+                Debug.Log(element.ToString());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
