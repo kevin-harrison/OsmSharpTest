9b29d2a4614a05799e8d9a64b793179095c8b1b6
Kevin Harrison <kevin@harrisoncentral.com>
2020-02-12 21:08:53 +0100

    Added way scraping example

--- Assets/OsmTest.cs
+++ Assets/OsmTest.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq; // for from clauses
 using UnityEngine;
 using OsmSharp.Streams;
+using OsmSharp.Complete;
+
+
 
 public class OsmTest : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        using (var fileStream = new FileInfo(@".\Assets\map.osm").OpenRead())
+        CompleteWay[] buildingWays = GetBuildings(@".\Assets\map.osm");
+        foreach (CompleteWay way in buildingWays)
         {
-            var source = new XmlOsmStreamSource(fileStream);
-            foreach (var element in source)
-            {
-                Debug.Log(element.ToString());
-            }
+            Debug.Log(way.Id);
+            Debug.Log(way.Tags);
+            Debug.Log($"Nodes: {way.Nodes.Length}");
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Gets building ways from the osm file
+    private CompleteWay[] GetBuildings(string osmFilePath)
     {
-        
+        using (var fileStream = new FileInfo(osmFilePath).OpenRead())
+        {
+            XmlOsmStreamSource source = new XmlOsmStreamSource(fileStream);
+
+            // Get all building ways and nodes 
+            var buildings = from osmGeo in source
+                            where osmGeo.Type == OsmSharp.OsmGeoType.Node ||
+                            (osmGeo.Type == OsmSharp.OsmGeoType.Way && osmGeo.Tags != null && osmGeo.Tags.ContainsKey("building"))
+                            select osmGeo;
+
+            // Should filter before calling ToComplete() to reduce memory usage
+            var completes = buildings.ToComplete(); // Create Complete objects (for Ways gives them a list of Node objects)
+            var ways = from osmGeo in completes
+                       where osmGeo.Type == OsmSharp.OsmGeoType.Way
+                       select osmGeo;
+            CompleteWay[] completeWays = ways.Cast<CompleteWay>().ToArray();
+            return completeWays;
+        }
     }
 }
